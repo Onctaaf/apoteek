c6caa1117340b638767a84051159a711cfa93a33
root <martijn.totte@gmail.com>
2018-05-29 15:05:43 +0200

    Alle vragen er in

--- apoteek/resources/views/home.blade.php
+++ apoteek/resources/views/home.blade.php
@@ -6,14 +6,130 @@
         <div class="col-md-8">
 
                   <section class="scrollclass">
-                    kaas
+                    <form action="/action_page.php" method="get">
+                     Voornaam: <input type="text" name="fname"><br>
+                     Achternaam: <input type="text" name="lname"><br>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         <p>Heeft u last van hoofdpijn?</p>
+                         <input type="radio" name="hoofdpijn" value="Ja">Ja<br>
+                         <input type="radio" name="hoofdpijn" value="Nee">Nee<br>
+                         <input type="radio" name="hoofdpijn" value="Beetje">Beetje<br>
+                       </div>
+                     </div>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         <p>Wat voor hoofdpijn?</p>
+                         <input type="radio" name="wathoofdpijn" value="Kloppend">Kloppend<br>
+                         <input type="radio" name="wathoofdpijn" value="Bonkend">Bonkend<br>
+                         <input type="radio" name="wathoofdpijn" value="Stekend">Stekend<br>
+                       </div>
+                     </div>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         <p>Bent u laatst vaak moe?</p>
+                         <input type="radio" name="moe" value="Ja">Ja<br>
+                         <input type="radio" name="moe" value="Nee">Nee<br>
+                         <input type="radio" name="moe" value="Beetje">Beetje<br>
+                       </div>
+                     </div>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         <p>Heeft u een tekenbeet opgemerkt?</p>
+                         <input type="radio" name="opgemerkt" value="Ja">Ja<br>
+                         <input type="radio" name="opgemerkt" value="Nee">Nee<br>
+                         <input type="radio" name="opgemerkt" value="Twijfel">Twijfel<br>
+                       </div>
+                     </div>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         <p>Wanneer heeft u de tekenbeet opgemerkt?</p>
+                         <input type="radio" name="wanopgemerkt" value="Binnen24">Binnen 24 uur na de beet<br>
+                         <input type="radio" name="wanopgemerkt" value="Na24">Na 24 na de beet<br>
+                         <input type="radio" name="wanopgemerkt" value="Twijfel">Ik weet het niet<br>
+                       </div>
+                     </div>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         <p>Heeft u de teek verweiderd?</p>
+                         <input type="radio" name="verweiderd" value="Ja">Ja<br>
+                         <input type="radio" name="verweiderd" value="Nee">Nee<br>
+                         <input type="radio" name="verweiderd" value="Beetje">I tried<br>
+                       </div>
+                     </div>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         <p>Wilt u een informatie filmpje hoe je een teek verweiderd?</p>
+                         <input type="radio" name="filmpje" value="Ja">Ja<br>
+                         <input type="radio" name="filmpje" value="Nee">Nee<br>
+                         <input type="radio" name="filmpje" value="Later">Ja, na deze vragenlijst<br>
+                       </div>
+                     </div>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         <p>Heeft u een rode kring?</p>
+                         <input type="radio" name="kring" value="Ja">Ja<br>
+                         <input type="radio" name="kring" value="Nee">Nee<br>
+                         <input type="radio" name="kring" value="Beetje">Misschien<br>
+                       </div>
+                     </div>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         <p>Heeft u een vorm van Lyme?</p>
+                         <input type="radio" name="uLyme" value="Ja">Ja<br>
+                         <input type="radio" name="uLyme" value="Nee">Nee<br>
+                         <input type="radio" name="uLyme" value="Beetje">Misschien<br>
+                       </div>
+                     </div>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         <p>Hoe oud bent u?</p>
+                         <input type="number" name="age" value="">
+                       </div>
+                     </div>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         <p>Upload de foto van uw tekenbeet</p>
+                         <input type="radio" name="fotoLoad" value="Ja">Ja<br>
+                         <input type="radio" name="fotoLoad" value="Nee">Nee<br>
+                         <br>
+                         <input type="file" name="fileToUpload" id="fileToUpload">
+                       </div>
+                     </div>
+
+                     <div id="fullSizeVraag">
+                       <div id="inMiddenVraag">
+                         E-mail van huisarts: <input type="text" name="fname"><br>
+                         Uw E-mail: <input type="text" name="lname"><br>
+                         <input type="submit" value="Submit">
+                       </div>
+                     </div>
+
+
+                     <br>
+                     <br>
+                     <br>
+                     <br>
+
+                  </form>
                   </section>
-                  <section class="scrollclass">
+                  <!-- <section class="scrollclass">
                     worst
                   </section>
                   <section class="scrollclass">
                     brood
-                  </section>
+                  </section> -->
                 </div>
     </div>
 </div>
