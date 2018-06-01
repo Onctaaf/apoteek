c7af270b20f40a2071a79f254b1b9a13e9a4d561
root <martijn.totte@gmail.com>
2018-06-01 13:19:33 +0200

    Laatste push ging fout

--- apoteek/resources/views/layouts/app.blade.php
+++ apoteek/resources/views/layouts/app.blade.php
@@ -37,9 +37,11 @@
       <div id="navcontainer" class="sticky">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel sticky tryFont">
             <div class="container">
+              <div class = "deLinks">
                 <a class="navbarLink" href="https://www.pornhub.com">
                     Apoteek
                 </a>
+              </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
