0e0c63a2ae7a522b051d7a343b6554f3a7f0b3be
root <martijn.totte@gmail.com>
2018-06-05 12:31:38 +0200

    Bovenbalk werkt laden

--- apoteek/public/js/kaas.js
+++ apoteek/public/js/kaas.js
@@ -9,18 +9,33 @@ function scrollup(){
   console.log("zijn geleverd");
   $.scrollify.previous();
 }
+var scroll = parseInt(window.innerHeight);
+
+var helemaal = document.body.scrollHeight;
+var temp = helemaal/100;
+var positie = scroll/temp;
+var parseIntergert = parseInt(positie);
+$('#inhoudLine').css("width", ""+parseIntergert+"%");
 
 $(window).scroll(function (event) {
-  var scroll = $(window).scrollTop();
-  console.log(scroll);
-  console.log(document.body.scrollHeight);
+  var scroll = $(window).scrollTop();//Bottom
+  // console.log(scroll);
+  // console.log(document.body.scrollHeight);
+
+  scroll = scroll + parseInt(window.innerHeight);
 
   var helemaal = document.body.scrollHeight;
   var temp = helemaal/100;
   var positie = scroll/temp;
   var parseIntergert = parseInt(positie);
+  // console.log(parseIntergert);
+  // console.log("Ding", parseIntergert);
+
   console.log(parseIntergert);
-  console.log("Ding", parseIntergert);
+
+  if(parseIntergert > 100){
+    parseIntergert = 100;
+  }
 
   $('#inhoudLine').css("width", ""+parseIntergert+"%");
 
