ea348a6334cf078df0163a4ff0a53d1486557d86
kevin <nielsvanbrenk@gmail.com>
2018-06-06 12:44:37 +0200

    kaas in mn hol

--- apoteek/resources/views/home.blade.php
+++ apoteek/resources/views/home.blade.php
@@ -32,7 +32,7 @@
                        <div class="inMiddenVraag">
                        <img onclick="scrollup()" src="{{ asset('images/pijl.svg')}}" class="pijlup">
                          <p>Wat voor hoofdpijn?</p>
-                         <input type="radio" name="wathoofdpijn" value="Kloppend">Kloppend<br>
+                         <input type="radio" name="wathoofdpijn" value="Kloppend" required>Kloppend<br>
                          <input type="radio" name="wathoofdpijn" value="Bonkend">Bonkend<br>
                          <input type="radio" name="wathoofdpijn" value="Stekend">Stekend<br>
                          <img onclick="scrolldown()" src="{{ asset('images/pijl.svg')}}" class="pijldown">
@@ -43,7 +43,7 @@
                        <div class="inMiddenVraag">
                        <img onclick="scrollup()" src="{{ asset('images/pijl.svg')}}" class="pijlup">
                          <p>Bent u laatst vaak moe?</p>
-                         <input type="radio" name="moe" value="Ja">Ja<br>
+                         <input type="radio" name="moe" value="Ja" required>Ja<br>
                          <input type="radio" name="moe" value="Nee">Nee<br>
                          <input type="radio" name="moe" value="Beetje">Beetje<br>
                          <img onclick="scrolldown()" src="{{ asset('images/pijl.svg')}}" class="pijldown">
@@ -54,7 +54,7 @@
                        <div class="inMiddenVraag">
                        <img onclick="scrollup()" src="{{ asset('images/pijl.svg')}}" class="pijlup">
                          <p>Heeft u een tekenbeet opgemerkt?</p>
-                         <input type="radio" name="opgemerkt" value="Ja">Ja<br>
+                         <input type="radio" name="opgemerkt" value="Ja" required>Ja<br>
                          <input type="radio" name="opgemerkt" value="Nee">Nee<br>
                          <input type="radio" name="opgemerkt" value="Twijfel">Twijfel<br>
                          <img onclick="scrolldown()" src="{{ asset('images/pijl.svg')}}" class="pijldown">
@@ -67,7 +67,7 @@
                          <p>Wanneer heeft u de tekenbeet opgemerkt?</p>
                          <input type="radio" name="wanopgemerkt" value="Binnen24">Binnen 24 uur na de beet<br>
 
-                        <input type="radio" name="wanopgemerkt" value="Na24">Na 24 na de beet<br>
+                        <input type="radio" name="wanopgemerkt" value="Na24" required>Na 24 na de beet<br>
                          <input type="radio" name="wanopgemerkt" value="Twijfel">Ik weet het niet<br>
                          <img onclick="scrolldown()" src="{{ asset('images/pijl.svg')}}" class="pijldown">
                        </div>
@@ -77,7 +77,7 @@
                        <div class="inMiddenVraag">
                        <img onclick="scrollup()" src="{{ asset('images/pijl.svg')}}" class="pijlup">
                          <p>Heeft u de teek verwijderd?</p>
-                         <input type="radio" name="verweiderd" value="Ja">Ja<br>
+                         <input type="radio" name="verweiderd" value="Ja" required>Ja<br>
                          <input type="radio" name="verweiderd" value="Nee">Nee<br>
                          <input type="radio" name="verweiderd" value="Beetje">I tried<br>
                          <img onclick="scrolldown()" src="{{ asset('images/pijl.svg')}}" class="pijldown">
@@ -88,9 +88,8 @@
                        <div class="inMiddenVraag">
                        <img onclick="scrollup()" src="{{ asset('images/pijl.svg')}}" class="pijlup">
                          <p>Wilt u een informatie filmpje hoe je een teek verwijderd?</p>
-                         <input type="radio" name="filmpje" value="Ja">Ja<br>
-                         <input type="radio" name="filmpje" value="Nee">Nee<br>
                          <input type="radio" name="filmpje" value="Later">Ja, na deze vragenlijst<br>
+                         <input type="radio" name="filmpje" value="Nee">Nee<br>
                          <img onclick="scrolldown()" src="{{ asset('images/pijl.svg')}}" class="pijldown">
                        </div>
                      </div>
@@ -99,7 +98,7 @@
                        <div class="inMiddenVraag">
                        <img onclick="scrollup()" src="{{ asset('images/pijl.svg')}}" class="pijlup">
                          <p>Heeft u een rode kring?</p>
-                         <input type="radio" name="kring" value="Ja">Ja<br>
+                         <input type="radio" name="kring" value="Ja" required>Ja<br>
                          <input type="radio" name="kring" value="Nee">Nee<br>
                          <input type="radio" name="kring" value="Beetje">Misschien<br>
                          <img onclick="scrolldown()" src="{{ asset('images/pijl.svg')}}" class="pijldown">
@@ -110,7 +109,7 @@
                        <div class="inMiddenVraag">
                        <img onclick="scrollup()" src="{{ asset('images/pijl.svg')}}" class="pijlup">
                          <p>Heeft u een vorm van Lyme?</p>
-                         <input type="radio" name="uLyme" value="Ja">Ja<br>
+                         <input type="radio" name="uLyme" value="Ja" required>Ja<br>
                          <input type="radio" name="uLyme" value="Nee">Nee<br>
                          <input type="radio" name="uLyme" value="Beetje">Misschien<br>
                          <img onclick="scrolldown()" src="{{ asset('images/pijl.svg')}}" class="pijldown">
@@ -121,7 +120,7 @@
                        <div class="inMiddenVraag">
                        <img onclick="scrollup()" src="{{ asset('images/pijl.svg')}}" class="pijlup">
                          <p>Hoe oud bent u?</p>
-                         <input type="number" name="age" value="">
+                         <input type="number" name="age" value="" required>
                          <br>
                          <img onclick="scrolldown()" src="{{ asset('images/pijl.svg')}}" class="pijldown">
                        </div>
@@ -130,8 +129,8 @@
                      <div class="fullSizeVraag test werk_container colorpad" data-section-name="werk_container">
                        <div class="inMiddenVraag">
                        <img onclick="scrollup()" src="{{ asset('images/pijl.svg')}}" class="pijlup">
-                         <p>Upload de foto van uw tekenbeet</p>
-                         <input type="radio" name="fotoLoad" value="Ja">Ja<br>
+                         <p>Zou u een foto willen uploaden van de tekenbeet?</p>
+                         <input type="radio" name="fotoLoad" value="Ja" required>Ja<br>
                          <input type="radio" name="fotoLoad" value="Nee">Nee<br>
                          <br>
                          <input type="file" name="fileToUpload" id="fileToUpload"><br>
@@ -142,8 +141,8 @@
                      <div class="fullSizeVraag test werk_container colorpad" data-section-name="werk_container">
                        <div class="inMiddenVraag">
                        <img onclick="scrollup()" src="{{ asset('images/pijl.svg')}}" class="pijlup">
-                         <p><input type="text" name="fname" placeholder="E-mail huisarts" style="width: 40%;"><br>
-                         <input type="text" name="lname" placeholder="eigen E-mail" style="width: 40%;"><br></p>
+                         <p><input type="text" name="fname" placeholder="E-mail huisarts" style="width: 40%;" required><br>
+                         <input type="text" name="lname" placeholder="eigen E-mail" style="width: 40%;" required><br></p>
                          <input type="submit" value="Submit" class="submitButton" style="width: 40%;"><br>
                          <img onclick="scrolldown()" src="{{ asset('images/pijl.svg')}}" class="pijldown">
                        </div>
@@ -164,7 +163,6 @@
                   </section> -->
                 </div>
                 <div>
-                  hallo
                 </div>
 
                 <script src="http://cdn.jsdelivr.net/jquery.easing/1.3/jquery.easing.1.3.js"></script>
