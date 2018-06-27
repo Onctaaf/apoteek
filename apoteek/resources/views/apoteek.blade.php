898c0b05dadd03c3d99eaded257c9eb26682f9d5
bobschaart <bobschaart@outlook.com>
2018-06-27 12:43:44 +0200

    Bob's kaasjes zending

--- apoteek/resources/views/apoteek.blade.php
+++ apoteek/resources/views/apoteek.blade.php
@@ -7,20 +7,64 @@
 
                   <section class="scrollclass">
                     <div class="apoteekHome">
-                      <p class="titelText" >Apoteek</p>
+                      <p class="titelText" >Wat is de ziekte van Lyme?</p>
                       <p class="insideText">
-                      Lorem ipsum dolor sit amet, ut vis ludus laoreet referrentur.<span class="highlightSpan"> Mandamus</span> corrumpit elaboraret pro ut. Vim posse verterem an, postea gloriatur in quo, regione utroque nec no. Ea ceteros mediocrem interpretaris eum.<br><br>
-
-Doctus option verterem his ex. Suas<span class="highlightSpan"> deterruisset</span> ut eam. Mel ea tantas constituto accommodare. rip lorus kip. Ne purto iisque legimus vel, id nam nisl fabulas.<br><br>
-
-Postea luptatum ea vis, quo et partiendo suavitate, liber movet <span class="highlightSpan"> interpretaris </span> quo at. Cu nec tempor vidisse, quo fugit mucius pertinax ut. Vitae meliore pericula an eum. Luptatum mandamus ei vix, duo tibique accusam mediocritatem ne. Mea cu expetenda signiferumque, pro mentitum tincidunt ea. An omnes blandit est, sit at munere legimus suavitate.
-</p>
+                        De ziekte van Lyme is een infectieziekte die veroorzaakt wordt door de bacterie Borrelia burgdorferi (Bb). Het is een spiraalvormige bacterie die men een spirocheet noemt. In 1981 ontdekte Willy burgdorfer een nieuwe bacterie in teken, een bacterie die behoort tot het geslacht Borrelia. Vandaar dat de bacterie Borrelia burgdorferi heet. Niet lang daarna werd ook aangetoond dat deze bacterie de veroorzaker van de ziekte van Lyme is.
+                      </p>
                     </div>
                     <div class="apoteekHome">
-                      <p class="titelText" >Over</p>
-                      <p class="insideText">No sed soluta accusata. In cum eius <span class="highlightSpan">complectitur</span>, mea errem euismod utroque ad. Mei tollit propriae id. An sed oblique platonem sapientem. In homero neglegentur eos, vim quem postea intellegebat an, habeo adipisci id sit.<br><br>
-
-Has quaeque veritus appetere ne, pro inani dolore elaboraret ea, velit dolore per et. Quas <span class="highlightSpan">conclusionemque</span> eam ea. Ex audiam aeterno singulis sea, te eos reque tollit. Sed nibh congue consul at. His nonumes minimum at. At velit populo instructior vim, ut movet fabellas lobortis est. Cum epicuri deterruisset conclusionemque at, sed te augue tempor sensibus, in omnes nusquam corrumpit vis.
+                      <p class="titelText" >Waarom noemt men het de ziekte van Lyme?</p>
+                      <p class="insideText">
+                        De ziekte van Lyme (spreek uit als "Lijm") is vernoemd naar het stadje Old Lyme in de Verenigde Staten, waar de ziekte in de jaren '70 werd (her)ontdekt en onderzocht.
+Eigenlijk is het een verwarrende benaming, alsof ene Dr. Lyme de ziekte zou hebben ontdekt. Maar de ziekte van Lyme is dus niet vernoemd naar een persoon! Vandaar dat de ziekte ook wel Lyme-ziekte of lymeziekte wordt genoemd, vergelijkbaar met het Engelse "Lyme disease". Medici en wetenschappers noemen het vaak Lyme-borreliose of Borreliose, hiermee verwijzend naar de bacterie. In Duitsland noemt men het ook meestal Borreliose. Op het de website is het wel makkelijk om het kortweg Lyme te noemen.
+                      </p>
+                    </div>
+                    <div class="apoteekHome">
+                      <p class="titelText" >Hoe raakt men geïnfecteerd met de Lyme-bacterie?</p>
+                      <p class="insideText">
+                        De Lyme-bacterie wordt meestal overgedragen door de beet van een geïnfecteerde teek. Teken zijn kleine parasieten die vooral voorkomen in bossen en velden. De teek hecht zich vast in de huid en zuigt zich vol met bloed. Als de teek is geïnfecteerd, dan kunnen de Lyme-bacteriën worden overgedragen naar de mens.
+Ook is aangetoond dat een zwangere vrouw die geïnfecteerd is met de Lyme-bacterie, deze kan overdragen op het ongeboren kind.
+Andere besmettingswegen die door sommigen worden vermoed, zijn borstvoeding, menselijk en dierlijk contact, bloedzuigende insecten (muggen, vliegen, vlooien), bloedtransfusies en transplantaties, maar deze besmettingswegen zijn (nog) niet wetenschappelijk aangetoond.
+                      </p>
+                    </div>
+                    <div class="apoteekHome">
+                      <p class="titelText" >Wat zijn de symptomen van de ziekte van Lyme?</p>
+                      <p class="insideText">
+                        Vroege symptomen zijn een rode ring/vlek genaamd erythema migrans (EM) op de plaats van de tekenbeet, griepachtige klachten zoals hoofdpijn, stijve nek, koorts, spierpijnen en vermoeidheid. Deze symptomen kunnen uitblijven.
+  Later kunnen diverse symptomen ontstaan. De ziekte van Lyme is een multi-systeem ziekte. Het kan leiden tot o.a. neurologische (zenuwstelsel/hersenen), dermatologische (huid), reumatologische (spieren en gewrichten), cardiologische (hart), opthalmologische (ogen) en psychiatrische klachten.
+                      </p>
+                    </div>
+                    <div class="apoteekHome">
+                      <p class="titelText" >Hoe wordt de diagnose "de ziekte van Lyme" gesteld?</p>
+                      <p class="insideText">
+                        De ziekte van Lyme is een klinische diagnose. Dit betekent dat de arts de diagnose moet stellen op basis van de ziektegeschiedenis en symptomen. Bij het zien van een EM zal de arts de diagnose "ziekte van Lyme" stellen. Zonder een EM is de diagnose moeilijker te stellen. Vaak worden dan testen gebruikt. Deze testen zijn ter ondersteuning van de diagnose. Er kan niet een diagnose gesteld worden op grond van de uitslag van de test alleen. De testen kunnen om verschillende redenen zowel fout positieve als fout negatieve uitslagen geven.
+                      </p>
+                    </div>
+                    <div class="apoteekHome">
+                      <p class="titelText" >Hoe wordt de ziekte van Lyme behandeld?</p>
+                      <p class="insideText">
+                        De ziekte van Lyme kan succesvol worden behandeld met antibiotica als dit vroeg na de besmetting gebeurt. Als men al langere tijd is besmet, wordt het moeilijker om de ziekte succesvol te behandelen. Er is onenigheid over welke behandeling het beste is. De duur, de doses en te gebruiken antibiotica verschilt per arts. De ILADS (een internationale Lyme organisatie), adviseert bij een EM al 6 weken in hoge dosis. Wanneer men al langere tijd is geïnfecteerd, adviseert de ILADS zelfs maanden tot jaren antibiotica, net zolang tot de klachten wegblijven.
+                      </p>
+                    </div>
+                    <div class="apoteekHome">
+                      <p class="titelText" >Wat zijn co-infecties?</p>
+                      <p class="insideText">
+                        De ziekte van Lyme is niet de enige tekenziekte. Teken kunnen behalve Borrelia burgdorferi ook nog drager zijn van andere ziekteverwekkers. Met noemt deze vaak co-infecties. Wereldwijd komen diverse co-infecties voor, waarvan ook enkele in Europa. Welke co-infecties onder teken in Nederland voorkomen en in welke mate is onduidelijk.
+                      </p>
+                    </div>
+                    <div class="apoteekHome">
+                      <p class="titelText" >Hoe lang bestaat de ziekte van Lyme?</p>
+                      <p class="insideText">
+                        De oudst bekende beschrijving van een geval van de ziekte van Lyme stamt uit 1883. In dat jaar beschreef de Duitse arts Alfred Buchwald een huidaandoening bij een patiënt dat tegenwoordig bekend staat als acrodermatitis chronica atrophicans(ACA). Deze huidaandoening komt voor bij late Lyme.
+In 1884 werden in Hongarije teken die aan een kat vast zaten, verzameld en bewaard. Meer dan een eeuw later wordt ontdekt dat ze geïnfecteerd waren met Borrelia burgdorferi (Bb). Dit is het oudst bekende geval van infectie.
+Waarschijnlijk bestaat de ziekte van Lyme al veel langer.
+                      </p>
+                    </div>
+                    <div class="apoteekHome">
+                      <p class="titelText" >Kunnen huisdieren ook de ziekte van Lyme krijgen?</p>
+                      <p class="insideText">
+                        Ja, honden en katten kunnen ook de ziekte van Lyme krijgen en paarden, vee en andere dieren waarschijnlijk ook. Honden en katten kunnen heel makkelijk tekenbeten krijgen en deze mee naar huis nemen. Behalve voor de hond of de kat, is er dus ook voor de mensen een risico om via huisdieren een tekenbeet te krijgen.
+                      </p>
                     </div>
                   </section>
                   <!-- <section class="scrollclass">
